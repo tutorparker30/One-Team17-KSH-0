792004908b329ff1feeb92989ec1c519b77cb460
KimSihyeon1525 <rlatlgus1525@naver.com>
2025-02-06 12:57:40 +0900

    뭔지 몰라도 이상해져서 다시 올림

--- Source/MyProject2/MyActor.cpp
+++ Source/MyProject2/MyActor.cpp
@@ -1,16 +1,9 @@
 // MyActor.cpp
 
 #include "MyActor.h"
-#include "Kismet/KismetSystemLibrary.h"
 
 AMyActor::AMyActor()
 {
-	PrimaryActorTick.bCanEverTick = false;
 }
 
-void AMyActor::BeginPlay()
-{
-	Super::BeginPlay();
 
-	UKismetSystemLibrary::PrintString(this, FString::Printf(TEXT("Source code written by a team member.")));
-}
